9392396713bee082020a1d8154da1d17a3d64519
mica <mica28368@gmail.com>
2024-08-26 12:07:23 -0300

    me quede haciendo la clase de jugadores

--- Clases/Jugadores.py
+++ Clases/Jugadores.py
@@ -1,1 +1,5 @@
 
+
+class Jugadores: 
+    def __init__(self):
+      self.jugadores = ['j1','j2','j3','j4']
---
+++ Clases/apuesta.py
@@ -0,0 +1,9 @@
+class Apuesta:
+    def ponerFichas (self, cuantas):
+      d
+    def tomarFichas (self, cuantas):
+      d
+    def tenerFicha (self):
+      d
+    def estaVacia(self):
+       d
